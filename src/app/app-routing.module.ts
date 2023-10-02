3336c091981701040ff79417ccf60ff498a4bbac
AndreyFCB1001 <andreysuarez14@gmail.com>
2023-10-02 08:37:32 -0500

    implementacion navegacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component'; //home
+import { DrinksComponent } from './app/menu/drinks/drinks.component'; //drinks
+import { CrepesWafflesComponent } from './app/menu/crepes-waffles/crepes-waffles.component';
 
-const routes: Routes = [];
+// definir rutas de navegacion
+
+const routes: Routes = 
+[
+  {path: '', redirectTo:'/', pathMatch:'full'}, // redireccion url vacios al home
+  {path: 'home', component: AppComponent},  //home
+  {path: 'drinks', component: DrinksComponent},  // enlace de drinks
+  {path: 'CreepsyWaffles', component: CrepesWafflesComponent}  // enlace de drinks
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
